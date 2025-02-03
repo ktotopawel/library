a99c34198c4f887f8a71f97a6860aa9cc1d81c94
ktotopawel <pawel.juszczak02@gmail.com>
2025-02-04 00:15:55 +0100

    fixed book card wrapping issues, slimmed-down header

--- script.js
+++ script.js
@@ -19,33 +19,63 @@ addBook('Wiedźmin 5', 'Sapkowski', 2005, 2);
 addBook('Wiedźmin 6', 'Sapkowski', 2012, 4);
 addBook('Wiedźmin 7', 'Sapkowski', 2018, 5);
 addBook('Wiedźmin 8', 'Sapkowski', 2025, 3);
+addBook('Wiedźmin', 'Sapkowski', 1999, 4.5);
+addBook('Wiedźmin 2', 'Sapkowski', 2001, 3);
+addBook('Wiedźmin 3', 'Sapkowski', 2002, 4.5);
+addBook('Wiedźmin 4', 'Sapkowski', 2003, 3);
+addBook('Wiedźmin 5', 'Sapkowski', 2005, 2);
+addBook('Wiedźmin 6', 'Sapkowski', 2012, 4);
+addBook('Wiedźmin 7', 'Sapkowski', 2018, 5);
+addBook('Wiedźmin 8', 'Sapkowski', 2025, 3);
+addBook('Wiedźmin', 'Sapkowski', 1999, 4.5);
+addBook('Wiedźmin 2', 'Sapkowski', 2001, 3);
+addBook('Wiedźmin 3', 'Sapkowski', 2002, 4.5);
+addBook('Wiedźmin 4', 'Sapkowski', 2003, 3);
+addBook('Wiedźmin 5', 'Sapkowski', 2005, 2);
+addBook('Wiedźmin 6', 'Sapkowski', 2012, 4);
+addBook('Wiedźmin 7', 'Sapkowski', 2018, 5);
+addBook('Wiedźmin 8', 'Sapkowski', 2025, 3);
 
 
 cards = document.querySelector('.content');
 
 for (let i = 0; i < myLibrary.length; i++) {
-    const card = document.createElement("div")
+    const card = document.createElement('div')
     card.className += "book";
-    title = document.createElement('h3');
+
+    const bookInfo = document.createElement('div');
+    bookInfo.className += 'book-info';
+
+    const title = document.createElement('h3');
     title.textContent = `${myLibrary[i].title}`;
     title.className += 'title';
-    card.appendChild(title);
-    author = document.createElement('p');
+    bookInfo.appendChild(title);
+
+    const yearAuthor = document.createElement('div');
+    yearAuthor.className += 'year-author';
+    
+    const author = document.createElement('p');
     author.textContent = `${myLibrary[i].author}`;
     author.className += 'author';
-    card.appendChild(author);
-    year = document.createElement('p');
+    yearAuthor.appendChild(author);
+
+    const year = document.createElement('p');
     year.textContent = `${myLibrary[i].year}`;
     year.className += 'year';
-    card.appendChild(year);
-    rating = document.createElement('p');
+    yearAuthor.appendChild(year);
+
+    const rating = document.createElement('p');
     ratingSpan = document.createElement('span');
     ratingSpan.textContent = `${myLibrary[i].rating}`;
     rating.appendChild(ratingSpan);
     rating.className += 'rating';
-    card.appendChild(rating);
+    bookInfo.appendChild(rating);
 
     card.style.filter = `hue-rotate(${Math.random() * 360}deg)`;
+
+    bookInfo.appendChild(yearAuthor);
+
+    card.appendChild(bookInfo);
 
     cards.appendChild(card);
 }
